5483e02a6de203928533e4437e706e0d264b5ea1
frun36 <franek.urbanski@gmail.com>
2024-01-06 13:20:42 +0100

    Added motion blur

--- src/wasm/wheel.rs
+++ src/wasm/wheel.rs
@@ -18,32 +18,34 @@ impl Wheel {
     }
 
     fn draw_with_position(&self, position: f32) {
-        super::draw_wheel(position, 1.);
+        super::draw_wheel(position, 0.3);
     }
 
     pub fn update_position(&mut self, elapsed: Duration) {
-        // let mut new_position = self.position_buffer.get_last()
-        //     + self.omega * elapsed.as_secs_f32();
-        // while new_position > 2. * PI {
-        //     new_position -= 2. * PI;
-        // }
-        // self.position_buffer.insert(new_position);
-
-        self.position += elapsed.as_secs_f32() * self.omega;
-        while self.position > 2. * PI {
-            self.position -= 2. * PI;
+        if let Some(last_position) = self.position_buffer.get_last() {
+            let mut new_position = last_position + self.omega * elapsed.as_secs_f32();
+            while new_position > 2. * PI {
+                new_position -= 2. * PI;
+            }
+            self.position_buffer.insert(new_position);
+        } else {
+            self.position_buffer.insert(0.);
         }
+
+        // self.position += elapsed.as_secs_f32() * self.omega;
+        // while self.position > 2. * PI {
+        //     self.position -= 2. * PI;
+        // }
     }
 
     pub fn draw(&self) {
         super::clear();
-        // for position in &self.position_buffer.buff {
-        //     self.draw_with_position(*position);
-        // }
-        self.draw_with_position(self.position);
+        for position in &self.position_buffer.buff {
+            self.draw_with_position(*position);
+        }
+        // self.draw_with_position(self.position);
     }
 }
-
 
 struct WheelBuffer {
     buff: Vec<f32>,
@@ -67,11 +69,13 @@ impl WheelBuffer {
         }
     }
 
-    fn get_last(&self) -> f32 {
-        if self.begin == 0 {
-            self.buff[self.buff.len() - 1]
+    fn get_last(&self) -> Option<f32> {
+        if self.buff.is_empty() {
+            None
+        } else if self.begin == 0 {
+            Some(self.buff[self.buff.len() - 1])
         } else {
-            self.buff[self.begin - 1]
+            Some(self.buff[self.begin - 1])
         }
     }
 }
